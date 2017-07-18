d920b8af95aba1f1514162ae1a21fa6966145571
Victor Rodriguez <victor.irix@gmail.com>
2017-07-18 17:51:48 +0200

    Minor Fix

--- webapp/config/express.js
+++ webapp/config/express.js
@@ -28,7 +28,7 @@ module.exports = function(app){
     console.log(req.user);
     res.locals.user = req.user;
     next();
-  })
+  });
 
   app.use(passport.initialize());
   app.use(passport.session());
