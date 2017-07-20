caa87e879fcbb2cc5c6934b87eb1867eeb6ca430
Victor Rodriguez <victor.irix@gmail.com>
2017-07-20 14:05:55 +0200

    Added JS Folder

---
+++ webapp/public/javascript/mapDraw.js
@@ -0,0 +1,15 @@
+function initMap() {
+  var myLatLng = {lat: myCampaing.lat, lng: myCampaing.log};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+initMap();
