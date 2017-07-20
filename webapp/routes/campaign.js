4cb590d85d0769ccd923d4ca4cc8d645d199ceb5
Juan Macias <yo@soyjuanmacias.com>
2017-07-20 10:45:08 +0200

    Reviews and campaign options created

--- webapp/routes/campaign.js
+++ webapp/routes/campaign.js
@@ -13,5 +13,9 @@ router.get('/:id/delete', CampaignController.campaignDelete);
 router.get('/:id/update', CampaignController.campaignUpdateGet);
 router.post('/:id/update', CampaignController.campaignUpdatePost);
 
+router.post('/:id/request', CampaignController.campaignRequestPost);
+
+router.post('/:id/complete', CampaignController.campaignMarkAsCompletePost);
+
 
 module.exports = router;
